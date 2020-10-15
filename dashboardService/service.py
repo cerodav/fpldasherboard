badd6c188d92ca80eed582f5c4081d71e2e0050f
cerodav <sherinedavis@outlook.com>
2020-10-15 08:58:45 +0530

    Commiting v2

--- dashboardService/service.py
+++ dashboardService/service.py
@@ -1,3 +1,4 @@
+import tornado.wsgi
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.options import define, options
@@ -5,19 +6,23 @@ from tornado.web import Application
 from tornado.platform.asyncio import asyncio
 from dashboardService.handlers.dashboardRequestHandler import DashboardRequestHandler
 
+
 define('port', default=8086, help='Port to listen on')
 
-def main():
-    """Construct and serve the tornado application."""
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+# def main():
+"""Construct and serve the tornado application."""
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-    app = Application([
-        ('/api/.*', DashboardRequestHandler)
-    ])
-    http_server = HTTPServer(app)
-    http_server.listen(options.port)
-    print('Dashboard service listening on http://localhost:%i' % options.port)
-    IOLoop.current().start()
+app = Application([
+    ('/api/.*', DashboardRequestHandler)
+])
 
-if __name__ == '__main__':
-    main()
+application = tornado.wsgi.WSGIAdapter(app)
+
+http_server = HTTPServer(app)
+http_server.listen(options.port)
+print('Dashboard service listening on http://localhost:%i' % options.port)
+IOLoop.current().start()
+
+# if __name__ == '__main__':
+#     main()
